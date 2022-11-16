b3fa102f793e5e0456e38d986b969bf8a7ace772
rezaulislamtarek <mrezaulislam.t@gmail.com>
2022-11-16 18:54:30 +0600

    font family, hint color, font size added for input layer and auto complete textview

--- materialspinner/src/main/java/xyz/arifz/materialspinner/MaterialSpinner.kt
+++ materialspinner/src/main/java/xyz/arifz/materialspinner/MaterialSpinner.kt
@@ -2,6 +2,7 @@ package xyz.arifz.materialspinner
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
@@ -13,6 +14,8 @@ import android.widget.Filterable
 import android.widget.ListAdapter
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.HtmlCompat
 import com.google.android.material.textfield.TextInputLayout
 import xyz.arifz.materialspinner.ExtensionFunctions.dpToPx
 
@@ -202,6 +205,22 @@ class MaterialSpinner : TextInputLayout {
 
     fun onItemClickListener(listener: AdapterView.OnItemClickListener) {
         autoCompleteTextView.onItemClickListener = listener
+    }
+
+    fun setTextInputLayer(fontFamily: Int? = null, hintHtml: Int) {
+        typeface = fontFamily?.let { ResourcesCompat.getFont(context, it) }
+        val text = HtmlCompat.fromHtml(
+            context.getString(hintHtml) ,
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        this.hint = text
+    }
+
+    fun setAutoCompleteTextView(fontFamily: Int? = null , fontSizeDp:Int? = null, textColorCode: String? = null ) {
+        autoCompleteTextView.typeface = fontFamily?.let { ResourcesCompat.getFont(context, it) }
+        autoCompleteTextView.setTextColor(Color.parseColor(textColorCode))
+        fontSizeDp?.dpToPx()?.let { autoCompleteTextView.textSize = it.toFloat() }
+
     }
 
 }
